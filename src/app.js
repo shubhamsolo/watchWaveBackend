7360c01a9ccd8ec5d4d50084db6281cfbe77ba24
shubhamsolo <121370651+shubhamsolo@users.noreply.github.com>
2024-11-03 19:28:26 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,8 +7,11 @@ import userRouter from "./routes/user.routes.js";
 const app = express();
 
 // CORS configuration
-app.use(cors());
-
+app.use(cors({
+  origin: ["https://gleeful-sawine-4b61b7.netlify.app"], // Replace with your specific origin
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowing OPTIONS for preflight
+  credentials: true // Allows cookies and credentials
+}));
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
